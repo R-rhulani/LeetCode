e63c0c08e5d13351e52f0a4b6e201fdf54798276
Rhulani G Hlungwani <Zinjiva@icloud.com>
2021-07-20 13:07:31 +0200

    added hacker rank files

--- HackerRank/MAXIMUM DIFFERENCE/MAXIMUM DIFFERENCE/main.cpp
+++ HackerRank/MAXIMUM DIFFERENCE/MAXIMUM DIFFERENCE/main.cpp
@@ -1,14 +1,54 @@
-//
-//  main.cpp
-//  MAXIMUM DIFFERENCE
-//
-//  Created by Rhulani Hlungwani on 2021/07/19.
-//
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+      public:
+      int maximumDifference;
+    int difference = 0, count = 0;
+    
+    Difference(vector<int>arr)
+    {
+        elements = arr;
+        computeDifference();
+    }
+    
+    int computeDifference()
+    {
+        for(int i=0; i<elements.size(); i++)
+        {
+            count = 0;
+             while(count<elements.size())
+             {
+                 difference=elements[count]-elements[i];
+                if(difference < 0)
+                {
+                    difference*=-1;
+                }
+                if(difference>maximumDifference)
+                {
+                maximumDifference = difference;
+                }
+                count++;
+            }
+        }
+        return maximumDifference;
+    }
+    // Add your code here
+
+}; // End of Difference class
+
+int main()
+{
+    vector<int> array = {8, 19, 3, 2, 7};
+    Difference num(array);
+    cout<<num.maximumDifference<<endl;
     return 0;
 }
